4b12c54097c95066af20f42fff8e0b364564a66e
Javier Pérez <hola@javierperez.com>
2016-03-15 17:14:42 +0100

    CSV hook backend improvements

--- src/LandingPages/Hook/Csv.php
+++ src/LandingPages/Hook/Csv.php
@@ -13,9 +13,14 @@ class Csv extends Backend
      */
     public function exec()
     {
+        $data = array();
+
+        if ( $this->getConfig('add_timestamp') ) {
+            $data['timestamp'] = date('Y-m-d H:i:s');
+        }
+
         // MAP first_name=>NAME
         //  ---> CSV[first_name] = POST[NAME]
-        $data = array();
         foreach ( $this->getConfig('map', array()) as $csv_field => $post_field) {
             $data[$csv_field] = $this->getData($post_field);
         }
@@ -29,6 +34,19 @@ class Csv extends Backend
             // ...and save the field names as its first row
             fputcsv($f, array_keys($data));
         } else {
+            // Check if it has the same fields (header)
+            $f = fopen($file_path,'r');
+            $current_fields = fgetcsv($f);
+            fclose($f);
+            if ( $current_fields != array_keys($data) ) {
+                rename($file_path, $file_path.'-'.date('YmdHis'));
+
+                // If file does not exist, then create...
+                $f = fopen($file_path, 'w');
+                // ...and save the field names as its first row
+                fputcsv($f, array_keys($data));
+            }
+
             // If file exists, then open as append...
             $f = fopen($file_path, 'a');
         }
